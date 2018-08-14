2cf81e8d50769782e749fc6f93766ee1b8992772
RodrigoValeretto <rodrigovaleretto@usp.br>
2018-08-13 22:30:13 -0300

    Primeiro Commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+#########################################
+#  Calculadora Infinita em C 		 	#
+#  										#
+#  Aluno: Rodrigo Valeretto 			#
+#  NUSP: 10684792						#
+#  Engenharia de Computação 018			#
+#										#
+#########################################
+*/
+
+int main()
+{
+	
+
+
+	return 0;
+}
