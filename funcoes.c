5e0377939beadf36594db65b51bce73c55df7ddd
RodrigoValeretto <rodrigovaleretto@usp.br>
2018-08-15 18:12:24 -0300

    Função verificafinal feita mas não funciona

--- funcoes.c
+++ funcoes.c
@@ -8,7 +8,31 @@ Tipo_Lista * crialistas()
 	return elemento;
 }
 
-int somaentradas(int ent, Tipo_Lista *Elem, Tipo_Lista *Temp)
+void verificafinal(int tam, Tipo_Lista * Elem, Tipo_Lista * inicioE)	//Função que verifica o numero mais significativo da entrada
+{
+	for (int i = 0; i < tam; i++)
+	{
+		Elem = inicioE;
+		while(1)
+		{
+
+			if (Elem->valor == 0 && Elem->next == NULL)
+			{
+				free(Elem);
+				break;
+			}else{
+				Elem = Elem->next;
+				if(Elem->next == NULL)
+					{break;}
+			}
+		}
+
+	}
+	return;
+}
+
+
+int somaentradas(int ent, Tipo_Lista *Elem, Tipo_Lista *Temp)	//Função para receber e somar as entradas
 {
 	int i=0;						//Declaração de Variáveis
 	int j=0;
@@ -21,24 +45,35 @@ int somaentradas(int ent, Tipo_Lista *Elem, Tipo_Lista *Temp)
 	inicioT = Temp;					//Guardando o local para onde os
 	inicioE = Elem;					//ponteiros apontam inicialmente
 
-	for (j = 0; j < ent ; j++)
+	for (j = 0; j < ent ; j++)		//For utilizado para realizar a soma
 	{
 		printf("Digite o tamanho da entrada %d\n",j);
-		scanf("%d",&tam);
+		scanf("%d",&tam);			//Recebe o tamanho da entrada
 		
-		fflush(stdin);
-		for(i=1;i<tam;i++)
+		fflush(stdin);				//Limpa o buffer do teclado
+		printf("Digite o valor do binario:\n");
+		for(i = 0;i < tam; i++)		//For utilizado para colocar todos os elementos em uma lista encadeada	
 		{
-			printf("Digite o valor do binario:\n");
-			scanf("%c",&c);
+			scanf("%c",&c);			//Variavel de suporte criada para receber a string da entrada
 
-			Elem->valor = atoi(&c);
-			printf("%d\n", Elem->valor);
+			Elem->valor = atoi(&c);	//Função que transforma a entrada em int e atribui ao valor do elemento da lista
+			
+			if(i!=(tam-1))
+			{
+				Elem->next = (Tipo_Lista*)calloc(1,sizeof(Tipo_Lista));
+				Elem = Elem->next;
+			}
 
-			Elem = Elem->next;
 		}
 
+		verificafinal(tam, Elem, inicioE);
+
 		Elem = inicioE;
+		while(Elem != NULL)
+		{
+			printf("*%d", Elem->valor);
+			Elem = Elem->next;
+		}
 		
 		Temp->valor = Temp->valor + Elem->valor;
 
